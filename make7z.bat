42424108a03626cc9f2a11ea7371f53abbb61918
wss445566 <wss445566@gmail.com>
2014-07-20 13:01:41 +0000

    增加 make7z.bat 的檢查

--- make7z.bat
+++ make7z.bat
@@ -1,13 +1,33 @@
 @echo off
-del /q *.7z
 setlocal
-set d=c:\l2ph-mod
-cd /d %d%
+
+pushd c:\l2ph-mod
+if %errorlevel%==1 (
+  echo path not found
+  pause
+  exit /b 1
+)
+if not exist build\l2ph.exe (
+  echo l2.exe not found
+  pause
+  exit /b 2
+)
+>rev.tmp svnversion
+<rev.tmp set /p rev=
+del /q rev.tmp
+
+echo %rev% | findstr ":"
+if %errorlevel%==0 (
+  echo update svn first
+  exit /b 3
+)
+
+::del /q *.7z
 
 md l2ph-mod
 cd l2ph-mod
-copy /y "%d%\build\*.exe" .
-copy /y "%d%\build\*.dll" .
+copy /y "..\build\*.exe" .
+copy /y "..\build\*.dll" .
 md Logs
 md Plugins
 md Scripts
@@ -16,18 +36,16 @@ cd Settings
 md ru
 md en
 
-copy /y "%d%\build\settings\ru\*.ini" "ru"
-copy /y "%d%\build\settings\en\*.ini" "en"
-copy /y "%d%\build\settings\*.ini" "."
+copy /y "..\..\build\settings\ru\*.ini" "ru"
+copy /y "..\..\build\settings\en\*.ini" "en"
+copy /y "..\..\build\settings\*.ini" "."
 
 del /q windows.ini
 del /q options.ini
 
-cd /d %d%
->rev.tmp svnversion
-<rev.tmp set /p rev=
-del /q rev.tmp
+cd ..\..
 "C:\Program Files\7-Zip\7z" a -r l2ph-modr%rev%.7z l2ph-mod
 rd /s /q l2ph-mod
+popd
 endlocal
 
